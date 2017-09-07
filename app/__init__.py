1d856217266d1fd14b85e267fe38f05ac5735b3a
grace-kimotho <graycenjoki@gmail.com>
2017-09-07 22:08:12 +0300

    work on bugs

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from app import accounts, updatelist
 
 app = Flask(__name__, instance_relative_config=True)
 app.secret_key = "prettysecret"
+app.debug = True
     
 acc_object = accounts.User()
 createlist_object = updatelist.UpdateLists()
 
 from app import views
 
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
+# app.config.from_object('config')
+# app.config.from_pyfile('config.py')
+ 
