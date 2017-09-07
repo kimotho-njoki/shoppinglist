78732080f39f5758e8928a09130ca1d1104f78ce
grace-kimotho <graycenjoki@gmail.com>
2017-09-07 11:58:54 +0300

    fix .travis.yml

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,13 @@
 from flask import Flask
-from app import accounts, createlist 
-
-
+from app import accounts, updatelist 
 
 app = Flask(__name__, instance_relative_config=True)
 app.secret_key = "prettysecret"
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 	
-
 acc_object = accounts.User()
-createlist_object = createlist.UpdateLists()
+createlist_object = updatelist.UpdateLists()
 
 
 from app import views
--- app/accounts.py
+++ app/accounts.py
@@ -1,32 +1,28 @@
 import re
 
 class User:
-	def __init__(self):
-		self.user_details = {'grace':'welcome'}
-		self.extra_details = {}
+    def __init__(self):
+        self.user_details = {'grace':'welcome'}
+        self.extra_details = {}
 
-	def LogIn(self, username, password):
-		if username in self.user_details:
-			if self.user_details[username] == password:
-				return "Successfully Logged In"
-			else:
-				return "Incorrect Password"
-		else:
-			return "Account does not exist. Please Register."
-		
-
-	def Register(self, username, email, password, repassword):
-		if re.match("^[a-zA-Z0-9 _]*$", username):
+    def LogIn(self, username, password):
+        if username in self.user_details:
+            if self.user_details[username] == password:
+                return "Successfully Logged In"
+            else:
+                return "Incorrect Password"
+        else:
+            return "Account does not exist. Please Register."	
+    def Register(self, username, email, password, repassword):
+        if re.match("^[a-zA-Z0-9 _]*$", username):
 			if username in self.user_details:
 				return "Username already exists"
 			else:
-				if len(password) < 6 or password != repassword or password == '' or repassword=='':
+				if len(password) < 6 or password != repassword or password == '' or repassword == '':
 					return "Password Invalid"
 				else:
 					self.user_details[username] = password
 					self.extra_details[username] = [email, repassword]
 					return "Successfully signed up. You can now LogIn."
-		else:
+        else:
 			return "Username Invalid"
-
-					
