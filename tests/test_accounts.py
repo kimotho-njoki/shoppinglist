e4124bdc563eac124da7531e7c02b6a761793cc4
grace-kimotho <graycenjoki@gmail.com>
2017-09-08 01:12:56 +0300

    fix errors in pylint

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,3 +1,6 @@
+"""
+user authentication tests
+"""
 import unittest
 from app.accounts import User
 
@@ -10,34 +13,58 @@ class TestAccounts(unittest.TestCase):
     Test for registering with an already existing username
     Test for too short password
     """
+
     def setUp(self):
+        """
+        create object
+        """
         self.account_object = User()
-    
+
     def tearDown(self):
+        """
+        delete the object
+        """
         del self.account_object
 
-
     def test_success_LogIn(self):
+        """
+        test for user with an account
+        """
         result = self.account_object.LogIn('grace', 'welcome')
         self.assertEqual(result, "Successfully Logged In")
 
     def test_failed_LogIn(self):
+        """
+        test for user without account
+        """
         result = self.account_object.LogIn('betty', 'beatrice')
         self.assertEqual(result, "Account does not exist. Please Register.")
 
     def test_incorrect_pwd(self):
+        """
+        test for incorrect password used
+        """
         result = self.account_object.LogIn('grace', 'emphasis')
         self.assertEqual(result, "Incorrect Password")
 
     def test_success_registration(self):
+        """
+        test for successful registration
+        """
         result = self.account_object.Register('john', 'john@gmail.com', 'johnjohn', 'johnjohn')
         self.assertEqual(result, "Successfully signed up. You can now LogIn.")
 
     def test_rpt_username(self):
+        """
+        test for already existing username
+        """
         result = self.account_object.Register('grace', 'grayce@gmail.com', 'kimotho', 'kimotho')
         self.assertEqual(result, "Username already exists")
 
     def test_short_pwd(self):
+        """
+        test for short password used
+        """
         result = self.account_object.Register('hanah', 'hanah@gmail.com', 'hey', 'hey')
         self.assertEqual(result, "Password Invalid")
 
--- tests/test_listcreation.py
+++ tests/test_listcreation.py
@@ -1,3 +1,6 @@
+"""
+shoppinglist creation tests
+"""
 import unittest
 from app.updatelist import UpdateLists
 
@@ -8,28 +11,46 @@ class TestLists(unittest.TestCase):
     Test invalid list_name
     Test correct list_name
     """
+
     def setUp(self):
+        """
+        initialize object
+        """
         self.shoplist = UpdateLists()
-    
+
     def tearDown(self):
+        """
+        delete object
+        """
         del self.shoplist
 
     def test_namein_catalog(self):
+        """
+        tests creating shoppinglist from catalog
+        """
         result = self.shoplist.create("grocery")
         self.assertEqual(result, "Shoppinglist Created Successfully")
 
     def test_notin_catalog(self):
+        """
+        tests trying to create a shoppinglist from catalog that doesn't exist
+        """
         result = self.shoplist.create("bags")
         self.assertEqual(result, "Catalog Not Found")
 
     def test_invalid_name(self):
+        """
+        tests the use of unauthorized characters in shoppinglist name
+        """
         result = self.shoplist.create_own(".#")
         self.assertEqual(result, None)
 
     def test_correct_name(self):
+        """
+        tests the use of correct characters in shoppinglist name
+        """
         result = self.shoplist.create_own("soaps")
         self.assertEqual(result, "Own Shoppinglist Created")
 
 if __name__ == '__main__':
     unittest.main()
-
